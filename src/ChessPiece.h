aac2125c20ab44c959f02759fd6f1f3137241e93
mig <segoviano.miguel4@gmail.com>
2021-11-05 21:48:14 +0100

    Saved different pieces

--- src/ChessPiece.h
+++ src/ChessPiece.h
@@ -11,7 +11,9 @@
 class Piece
 {
 	public:
+		virtual ~Piece();
 		virtual void move() = 0;
+
 	private:
 		bool captured_m;
 };
@@ -20,9 +22,48 @@ class King : public Piece
 {
 	public:
 		void move() override;
+
 	private:
-}
+};
 
+class Queen : public Piece
+{
+	public:
+		void move() override;
 
+	private:
+};
+
+class Bishop : public Piece
+{
+	public:
+		void move() override;
+
+	private:
+};
+
+class Knight : public Piece
+{
+	public:
+		void move() override;
+
+	private:
+};
+
+class Rook : public Piece
+{
+	public:
+		void move() override;
+
+	private:
+};
+
+class Pawn : public Piece
+{
+	public:
+		void move() override;
+
+	private:
+};
 
 #endif /* CHESSPIECE_H_ */
